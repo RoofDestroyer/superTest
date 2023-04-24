627095aa7ad07bc0e3fe9c102c0f27e9d751b135
Nikita <1nikita7@mail.ru>
2023-04-24 17:49:51 +0300

    команды подгружаются без кнопки

--- myapp/src/component/test-api.jsx
+++ myapp/src/component/test-api.jsx
@@ -1,40 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const TestApi = () => {
   const [team, setTeam] = useState([]);
 
   const getTeam = () => {
-    // const options = {
-    //   method: "GET",
-    //   headers: {
-    //     "content-type": "application/octet-stream",
-    //     "X-RapidAPI-Key": "488f437511msh2d3854838388c55p13692cjsn135921cfebdf",
-    //     "X-RapidAPI-Host": "free-nba.p.rapidapi.com",
-    //   },
-    // };
-
-    // const url = "https://free-nba.p.rapidapi.com/teams?page=0";
-
-    fetch("https://free-nba.p.rapidapi.com/teams?page=0", {
+    const options = {
       method: "GET",
       headers: {
         "content-type": "application/octet-stream",
         "X-RapidAPI-Key": "488f437511msh2d3854838388c55p13692cjsn135921cfebdf",
         "X-RapidAPI-Host": "free-nba.p.rapidapi.com",
       },
-    })
+    };
+
+    const url = "https://free-nba.p.rapidapi.com/teams?page=0";
+
+    fetch(url, options)
       .then((response) => response.json())
       .then((response) => setTeam(response.data));
   };
 
+  useEffect(() => {
+    getTeam();
+  }, []);
+
   return (
     <div>
-      <button onClick={getTeam}>Клик</button>
+      {/* <button onClick={getTeam}>Клик</button> */}
       {team.map((el, i) => {
         return (
           <div key={el.id}>
             {" "}
-           {i+1} {el.abbreviation} {el.city}
+            {i + 1} {el.abbreviation} {el.city}
           </div>
         );
       })}
